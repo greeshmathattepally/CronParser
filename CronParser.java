b077e1fc310f55bf84e52b4e5b5b234c9bb2e36a
Thattepally Greeshma <thattepallygreeshma@gmail.com>
2023-08-03 10:34:46 +0530

    Changes to handle cases where invalid cron format is provided as input

--- CronParser.java
+++ CronParser.java
@@ -10,11 +10,21 @@ public class CronParser {
         String cronString = args[0];
         String[] fields = cronString.split("\\s+");
 
+        if (fields.length != 6) {
+            System.out.println("Invalid cron format");
+            return; 
+        }
+
         List<String> minute = expandField(fields[0], 0, 59);
         List<String> hour = expandField(fields[1], 0, 23);
         List<String> dayOfMonth = expandField(fields[2], 1, 31);
         List<String> month = expandField(fields[3], 1, 12);
         List<String> dayOfWeek = expandField(fields[4], 0, 7);
+
+        if (minute == null || hour == null || dayOfMonth == null || month == null || dayOfWeek == null) {
+            System.out.println("Invalid cron string format."+"min"+minute+"hour"+hour+"dayOfMonth"+dayOfMonth+"month"+month+"dayOfWeek"+dayOfWeek);
+            return;
+        }
 
         System.out.println("minute " + String.join(" ", minute));
         System.out.println("hour " + String.join(" ", hour));
@@ -29,7 +39,12 @@ public class CronParser {
         String[] elements = field.split(",");
 
         for (String element : elements) {
-            if (element.equals("*")) { // Handle asterisk (*) separately
+
+            if (!element.matches("[0-9\\-*/]+")) {
+                return null; 
+            }
+
+            if (element.equals("*")) { 
                 for (int i = min; i <= max; i++) {
                     result.add(String.valueOf(i));
                 }
@@ -38,6 +53,8 @@ public class CronParser {
                 int start = min;
                 int step = Integer.parseInt(stepElements[1]);
                 if (!stepElements[0].equals("*")) {
+                    if(start<min || start>max || step<min || step>max)
+                        return null;
                     start = Integer.parseInt(stepElements[0]);
                 }
                 for (int i = start; i <= max; i += step) {
@@ -49,6 +66,8 @@ public class CronParser {
                 String[] rangeElements = element.split("-");
                 int start = Integer.parseInt(rangeElements[0]);
                 int end = Integer.parseInt(rangeElements[1]);
+                if(start<min || start>max || end<min || end>max)
+                    return null;
                 for (int i = start; i <= end; i++) {
                     if (i >= min && i <= max) {
                         result.add(String.valueOf(i));
@@ -58,6 +77,9 @@ public class CronParser {
                 int value = Integer.parseInt(element);
                 if (value >= min && value <= max) {
                     result.add(element);
+                }
+                else{
+                    return null;
                 }
             }
         }
--- CronParserTest.java
+++ CronParserTest.java
@@ -8,6 +8,10 @@ public class CronParserTest {
         testExpandFieldWithSingleValue();
         testExpandFieldWithInvalidValue();
         testExpandFieldWithStepAndStartValue();
+        testStepValueNotInRange();
+        testNormalValuesNotInSpecifiedRange();
+        testInvalidCharacters();
+        testEndValueNotInRange();
     }
 
     private static void testExpandFieldWithAsterisk() {
@@ -36,8 +40,8 @@ public class CronParserTest {
 
     private static void testExpandFieldWithInvalidValue() {
         List<String> result = CronParser.expandField("70-80", 0, 59);
-        System.out.println("Expected: (empty list)");
-        System.out.println("Actual: " + String.join(" ", result));
+        System.out.println("Expected: Invalid cron format");
+        System.out.println("Actual: " + (result == null ? "Invalid cron format" : String.join(" ", result)));
     }
 
     private static void testExpandFieldWithStepAndStartValue() {
@@ -45,5 +49,30 @@ public class CronParserTest {
         System.out.println("Expected: 5 20 35 50");
         System.out.println("Actual: " + String.join(" ", result));
     }
+
+    private static void testStepValueNotInRange(){
+        List<String> result = CronParser.expandField("12/60", 0, 59);
+        System.out.println("Expected: Invalid cron format");
+        System.out.println("Actual: " + (result == null ? "Invalid cron format" : String.join(" ", result)));
+    }
+
+    private static void testNormalValuesNotInSpecifiedRange() {
+        List<String> result = CronParser.expandField("65", 0, 59);
+        System.out.println("Expected: Invalid cron format");
+        System.out.println("Actual: " + (result == null ? "Invalid cron format" : String.join(" ", result)));
+    }
+
+    private static void testInvalidCharacters() {
+        List<String> result = CronParser.expandField("*/15@@", 0, 59);
+        System.out.println("Expected: Invalid cron format");
+        System.out.println("Actual: " + (result == null ? "Invalid cron format" : String.join(" ", result)));
+    }
+
+    private static void testEndValueNotInRange() {
+        List<String> result = CronParser.expandField("10-70", 0, 59);
+        System.out.println("Expected: Invalid cron format");
+        System.out.println("Actual: " + (result == null ? "Invalid cron format" : String.join(" ", result)));
+    }
+
 }
 
